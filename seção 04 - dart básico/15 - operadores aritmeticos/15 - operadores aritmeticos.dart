a9aad372c4fdfe91341fc06a8030a8cac8fc5f4c
Luiz Gustavo Zanerato Evangelista <gustavozanerato@hotmail.com>
2022-10-06 23:43:04 -0300

    15 - operadores aritmeticos

---
+++ seção 04 - dart básico/15 - operadores aritmeticos/15 - operadores aritmeticos.dart
@@ -0,0 +1,22 @@
+void main(){
+  double n1 = 100.50;
+  double n2 = 52.20;
+  int n3 = 10;
+
+  double resultado =(n1 / n2) * n3;
+  print(resultado);
+
+// alterar valores de variaveis numericas
+n1 = n1 + 25;
+print(n1);
+
+n3 += 30;
+print(n3);
+
+n2 ++;
+print(n2);
+
+// resto de divisao
+print(n1 % 2);
+
+}
