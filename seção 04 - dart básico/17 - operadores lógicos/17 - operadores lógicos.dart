d60e5a1eb9214fa69b5709549483cb1d764f5e26
Luiz Gustavo Zanerato Evangelista <gustavozanerato@hotmail.com>
2022-10-13 13:00:03 -0300

    17 - operadores lógicos

---
+++ seção 04 - dart básico/17 - operadores lógicos/17 - operadores lógicos.dart
@@ -0,0 +1,13 @@
+void main(){
+  int n = 15;
+
+  // todos vão retornar um booleano
+  print(n == 15); 
+
+  print(n != 10); // diferente
+
+  print(n > 10); 
+
+
+  // and: retorna true se as duas condções são verdades
+}
