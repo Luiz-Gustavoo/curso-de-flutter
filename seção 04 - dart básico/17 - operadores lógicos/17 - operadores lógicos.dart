198e599f5d10035db9084d74ec3546b3761b43af
Luiz Gustavo Zanerato Evangelista <gustavozanerato@hotmail.com>
2022-10-13 22:02:14 -0300

    17 - operadores lógicos

--- seção 04 - dart básico/17 - operadores lógicos/17 - operadores lógicos.dart
+++ seção 04 - dart básico/17 - operadores lógicos/17 - operadores lógicos.dart
@@ -10,4 +10,15 @@ void main(){
 
 
   // and: retorna true se as duas condções são verdades
+  bool resultado1 =  30 < 15 && 10 > 5;
+  print(resultado1);
+  print(!resultado1);
+
+  // or: retorna true se uma das condições é verdade
+  bool resultado2 = 20 > 12 || 3 > 1;
+  print(resultado2);  // negação: resultado invertido
+  print(!resultado2);
+
+
+
 }
