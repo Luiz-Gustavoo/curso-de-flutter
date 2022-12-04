252d8d24cf34bb678d180fb5c2521c04ceeccb56
Luiz-Gustavoo <gustavo_luiz45@hotmail.com>
2022-12-04 20:47:13 -0300

    do while loop

---
+++ seção 04 - dart básico/22-do while loop/22- do while loop.dart
@@ -0,0 +1,13 @@
+void main() {
+  // do while executa o código primeiro e depois faz a vericação while
+  int n = 10;
+
+  while(n > 10){
+    print('olá');
+
+  }
+  do {
+    print('olá 2');
+  }  while(n < 20);
+
+}
