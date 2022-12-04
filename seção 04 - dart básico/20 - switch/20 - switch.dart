6d26d94c637e565c5e7ed2021632716c4d1b75d5
Luiz-Gustavoo <gustavo_luiz45@hotmail.com>
2022-12-04 17:36:12 -0300

    switch

---
+++ seção 04 - dart básico/20 - switch/20 - switch.dart
@@ -0,0 +1,21 @@
+void main() {
+    // switch também é usado para tomar decisões
+    // usando if
+    String carro = 'gol';
+
+    if(carro == 'gol') {
+        print('carro mais brabo de todos');
+      }
+
+    if(carro == 'azera') {
+      print('esse é de patrão');
+    }
+
+    if (carro == 'palio') {
+      print('uma bosta');
+
+    } 
+
+    // usando switch
+    
+}
