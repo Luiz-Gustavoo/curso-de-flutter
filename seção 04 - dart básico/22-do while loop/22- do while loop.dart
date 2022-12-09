d971f840f433c8b4d737ee9bf8a211023c0a05dc
Luiz-Gustavoo <gustavo_luiz45@hotmail..com>
2022-12-08 22:10:39 -0300

    do while loop

--- seção 04 - dart básico/22-do while loop/22- do while loop.dart
+++ seção 04 - dart básico/22-do while loop/22- do while loop.dart
@@ -1,11 +1,14 @@
 void main() {
-  // do while executa o código primeiro e depois faz a vericação while
+  
   int n = 10;
 
   while(n > 10){
     print('olá');
 
   }
+
+  // do while executa o código primeiro e depois faz a vericação while
+
   do {
     print('olá 2');
   }  while(n < 20);
---
+++ seção 04 - dart básico/23 - for loop/for loop.dart
@@ -0,0 +1,14 @@
+void main() {
+    // variavel, condição, incremento
+    for(int n1 = 0; n1 <= 10; n1++) {
+      for(int n2 = 10; n2 >= 0; n2--) {
+        print('n1 = $n1');  // roda o loop do n2. depois roda o loop do n1
+        print('n2 = $n2');
+    }
+    
+    }
+
+  
+
+
+}
