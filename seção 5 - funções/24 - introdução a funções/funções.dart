dcbba7e97fc2f0aba078aa06008d87f163646359
Luiz-Gustavoo <gustavo_luiz45@hotmail..com>
2022-12-15 22:07:54 -0300

    introdução a funções

---
+++ seção 5 - funções/24 - introdução a funções/funções.dart
@@ -0,0 +1,18 @@
+void main() {
+  // função: usada para repetir códigos
+  saudacao();
+  despedida();
+  
+
+  
+}
+void saudacao() {
+  print('ola ');
+  print('seja bem vindo');
+
+}
+
+void despedida() {
+  print('até logo');
+
+}
