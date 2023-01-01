afd10c6686dfc62a59b20c71452ce2e1b44c28b0
Luiz-Gustavoo <gustavo_luiz45@hotmail..com>
2023-01-01 17:43:35 -0300

    listas

---
+++ seção 06 - coleções/34 - listas/listas.dart
@@ -0,0 +1,17 @@
+void main() {
+  // coleções/listas: forma para organizar um conjunto de dados
+  List nomes = [
+    'luiz',
+    'gustavo'];
+  print(nomes);
+
+  List <String> mercado = [
+    'pão',
+    'ovo', 
+    'macarrão'];
+
+  print(mercado);
+  print(mercado.length);  // length: mostra a qtd de valores na lista
+  print(mercado[0]); // buscar um valor específico dentro da lista(índices)
+
+}
