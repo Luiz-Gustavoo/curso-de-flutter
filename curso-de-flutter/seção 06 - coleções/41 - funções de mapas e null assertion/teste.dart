e75b8f208e5d1720fb2db014422d60dcb8a80488
Luiz Gustavo Zanerato Evangelista <gustavozanerato@hotmail.com>
2023-02-11 19:47:35 -0300

    teste repositorio

--- curso-de-flutter/seção 06 - coleções/41 - funções de mapas e null assertion/teste.dart
+++ curso-de-flutter/seção 06 - coleções/41 - funções de mapas e null assertion/teste.dart
@@ -1,10 +1,3 @@
 void main() {
-List<Widget> _randomColors() {
- Random rnd = Random();
- return List.generate(25,
- (int i) => Container(
- color: Color.fromRGBO(
- rnd.nextInt(255), rnd.nextInt(255), rnd.nextInt(255), 1.0),
- ));
-}
+  print('teste');
 }
