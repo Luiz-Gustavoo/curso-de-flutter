1901032cd912048e54824cae9bc94deb5680e3f7
Luiz Gustavo Zanerato Evangelista <gustavozanerato@hotmail.com>
2023-02-16 20:28:33 -0300

    enumeradores

--- seção 07 - outros recursos essenciais/47 - enumeradores/47.dart
+++ seção 07 - outros recursos essenciais/47 - enumeradores/47.dart
@@ -1,7 +1,23 @@
 // enumerador: especificar valores limitados para uma variavel
-
+//                        0       1           2
 enum status_pagamento  {pago, pendente, reembolsado}
 void main() {
-  status_pagamento status = status_pagamento.pendente;
+  status_pagamento status = status_pagamento.pago;
   print(status);
+
+  switch (status) {
+
+    case status_pagamento.pago:
+      print('pagamento feito');
+      break;
+    case status_pagamento.pendente:
+      print('pagamento pendente');
+      break;
+    case status_pagamento.reembolsado:
+      print('pagamento reembolsado');
+      break;
+  }
+
+  // banco de dados não suportam enumeradores. eles precisam ser convertidos para número
+  print(status.index);
 }
