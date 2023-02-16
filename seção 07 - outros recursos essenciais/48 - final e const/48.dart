581dcc1061fb91e7d058c49ecf35c6bd77f3995b
Luiz Gustavo Zanerato Evangelista <gustavozanerato@hotmail.com>
2023-02-16 20:47:24 -0300

    constante e final

---
+++ seção 07 - outros recursos essenciais/48 - final e const/48.dart
@@ -0,0 +1,10 @@
+void main() {
+  // const: usado em variaveis que o valor não muda(compilação)
+const String nome = 'luiz';
+print('a constante $nome é constante e seu valor não pode ser alterado');
+
+
+// final: valores fixos em tepos de execução. pode ser atribuido depois
+final String nome2 = 'gustavo';
+
+}
